e06053d6c24957f6ac2074f1dd2b61565a344904
t2 <oo_ri@naver.com>
2021-01-18 02:55:16 +0900

    Simple TypeScript Code

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log('Hello TypeScript');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = 'DeveloperT2';
+const age = 33;
+const gender = 'male';
+const person = (name, age, gender) => {
+    console.log({ name, age, gender });
+};
+person(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log('Hello TypeScript');
+const name = 'DeveloperT2';
+const age = 33;
+const gender = 'male';
+
+const person = (name, age, gender?) => {
+  console.log({ name, age, gender });
+};
+
+person(name, age);
+
+export {};
