376464757c588c8dd3e04d140125527b2ad6e2b0
t2 <oo_ri@naver.com>
2021-01-18 15:22:39 +0900

    Creating a BlockChain Functions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import * as cryptoJS from 'crypto-js';
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ): string => {
+    return cryptoJS.SHA256(index + previousHash + data + timestamp).toString();
+  };
 
   constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
     this.index = index;
@@ -15,9 +26,18 @@ class Block {
 }
 
 const rootBlock: Block = new Block(0, '0401', '', 'TypeScript', 210118);
+const blockChain: Block[] = [rootBlock];
 
-const blockChain: [Block] = [rootBlock];
+const getBlockChain = (): Block[] => {
+  return blockChain;
+};
 
-console.log(blockChain);
+const getLatestBlock = (): Block => {
+  return blockChain[blockChain.length - 1];
+};
+
+const getNewTimeStamp = (): Number => {
+  return Math.round(new Date().getTime() / 1000);
+};
 
 export {};
