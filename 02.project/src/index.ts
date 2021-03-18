4b4e1997f8900e316e7b85a3557a5903fd2facdc
t2 <oo_ri@naver.com>
2021-03-19 01:56:46 +0900

    Update index.ts

--- 02.project/src/index.ts
+++ 02.project/src/index.ts
@@ -1,3 +1,27 @@
-import { testMakePerson } from './utils/makePerson';
+const MAX_AGE = 100;
+
+interface IPerson {
+  name: string;
+  age: number;
+}
+
+class Person implements IPerson {
+  constructor(public name: string, public age: number) {}
+}
+
+const makeRandomNumber = (max: number = MAX_AGE): number => {
+  return Math.ceil(Math.random() * max);
+};
+
+const makePerson = (name: string, age: number = makeRandomNumber()) => ({
+  name,
+  age,
+});
+
+const testMakePerson = (): void => {
+  const t2: IPerson = makePerson('t2');
+
+  console.log(t2);
+};
 
 testMakePerson();
