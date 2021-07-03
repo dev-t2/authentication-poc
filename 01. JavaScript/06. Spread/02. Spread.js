ecded5f1149995384a43bf76ee51b99b327c3ef3
t2 <oo_ri@naver.com>
2021-07-03 17:38:40 +0900

    01 ~ 03 Spread Update

---
+++ 01. JavaScript/06. Spread/02. Spread.js
@@ -0,0 +1,8 @@
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+
+console.log(array1);
+
+array1.push(...array2);
+
+console.log(array1);
---
+++ 01. JavaScript/06. Spread/03. Spread.js
@@ -0,0 +1,5 @@
+const array = [1, 2, 3];
+
+const maxValue = Math.max(...array);
+
+console.log(maxValue);
