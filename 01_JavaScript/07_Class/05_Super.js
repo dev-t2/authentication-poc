18eb7d49af2f5b2d43c4bb18b18055e43063d303
t2 <oo_ri@naver.com>
2021-07-04 14:45:53 +0900

    07_Class Folder and File Name Update

--- 01_JavaScript/07_Class/05_Super.js
+++ 01_JavaScript/07_Class/05_Super.js
@@ -3,12 +3,12 @@ class A {
     this.value1 = value1;
   }
 
-  getValue1() {
-    console.log(this.value1);
+  method() {
+    return this.value1;
   }
 
-  func() {
-    return this.value1;
+  methodA() {
+    console.log(this.value1);
   }
 }
 
@@ -19,19 +19,19 @@ class B extends A {
     this.value2 = value2;
   }
 
-  getValue2() {
-    console.log(this.value2);
-  }
-
-  func() {
-    const value1 = super.func();
+  method() {
+    const value1 = super.method();
 
     console.log(value1 + this.value2);
+  }
+
+  methodB() {
+    console.log(this.value2);
   }
 }
 
 const b = new B(1, 2);
 
-b.getValue1();
-b.getValue2();
-b.func();
+b.methodA();
+b.methodB();
+b.method();
