ffc6985ec601b9a28da998dad2983a83e6408e27
t2 <oo_ri@naver.com>
2021-07-06 23:55:55 +0900

    06_Generic and 01_Generic.ts Update

---
+++ 06_Generic/01_Generic.ts
@@ -0,0 +1,16 @@
+class Person {
+  name: string;
+}
+
+class Employee extends Person {
+  department: number;
+}
+
+class Animal {
+  breed: string;
+}
+
+const workers: Array<Person> = [];
+
+workers[0] = new Person();
+workers[1] = new Employee();
