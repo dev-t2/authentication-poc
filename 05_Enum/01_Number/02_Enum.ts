9008f5b020d38ca6d28bfe36be303b58386c66a9
t2 <oo_ri@naver.com>
2021-07-06 23:31:42 +0900

    01 ~ 03 Enum.ts Update

---
+++ 05_Enum/01_Number/02_Enum.ts
@@ -0,0 +1,11 @@
+enum Weekdays2 {
+  Monday = 1,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+console.log(Weekdays2.Friday);
---
+++ 05_Enum/01_Number/03_Enum.ts
@@ -0,0 +1,12 @@
+enum Weekdays3 {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+console.log(Weekdays3.Friday);
+console.log(Weekdays3[5]);
