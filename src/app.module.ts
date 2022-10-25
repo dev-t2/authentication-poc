b61e7941663f76db6bbf20bb549990e8951d1e28
dev-t2 <oo_ri@naver.com>
2022-10-25 23:15:19 +0900

    App Module Code Refactoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 
 import { AuthModule } from './auth/auth.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [AuthModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
