f1e87d558ae04bc913952b8c0a74e52a8c71fe01
dev-t2 <oo_ri@naver.com>
2022-10-25 23:13:37 +0900

    Auth Module Validate User Service Update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
+import { UsersService } from 'src/users/users.service';
+
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  validateUser(username: string, password: string) {
+    const user = this.usersService.findOne(username);
+
+    if (user && user.password === password) {
+      const { userId, username } = user;
+
+      return { userId, username };
+    }
+
+    return null;
+  }
+}
